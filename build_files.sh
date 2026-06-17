1579a44142bec7c2dab070a5de4f1b7878af75e3
alan-thoppil <alanthoppil.8@gmail.com>
2026-06-17 14:06:59 +0530

    fix: add --break-system-packages to bypass PEP 668 on Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Building project..."
-pip3 install -r counseling_app/requirements.txt
+pip3 install --break-system-packages -r counseling_app/requirements.txt
 python3 counseling_app/manage.py collectstatic --noinput --clear
